f51cd01fc2e7892676eae71c14cfd85e5e33289d
jamsluo <jamsluo@tencent.com>
2023-03-13 11:18:08 +0800

    init http client

--- convert_model/rm_glm_to_onnx.py
+++ convert_model/rm_glm_to_onnx.py
@@ -22,12 +22,12 @@ RM_model_path = "/search/ai/kaitongyang/RLHF_DEBUG/RM/reward_model_glm_10b/final
 
 RM_tokenizer = AutoTokenizer.from_pretrained(RM_model_path, trust_remote_code=True)
 RM_model = AutoModelForSequenceClassification.from_pretrained(RM_model_path, torchscript=True, trust_remote_code=True)
-RM_model = RM_model.half().eval()
+RM_model = RM_model.half().to(device)
 query_text = '什么人不能喝三七粉' + "[UNUSED1]"
 response_text = '服用三七粉期间,孕妇和儿童不宜使用。 三七粉是处方药,不是药品。 过量服用会引起中毒。'
 input = RM_tokenizer(query_text + response_text, max_length=512, padding="max_length", return_tensors="pt").to(device)
 print(input)
-print(RM_model(input['input_ids'], input['attention_mask']))
+# print(RM_model(input['input_ids'], input['attention_mask']))
 RM_model = RM_model.eval()  # 转换为eval模式
 inputs = (input['input_ids'], input['attention_mask'])  # 模型测试输入数据
 os.makedirs(f"model_store/{model_name}/1", exist_ok=True)
@@ -38,7 +38,7 @@ torch.onnx.export(
 	input_names=['input_ids', 'attention_mask', 'token_type_ids'],  # 输入节点名，每一个名称对应一个输入名
     output_names=['output'],  # 输出节点名，每一个名称对应一个输出名
 	opset_version=11,
-	dynamic_axes={'input_ids': {0: 'B', 1: 'C'}, 'attention_mask': {0: 'B', 1: 'C'}, 'token_type_ids': {0: 'B', 1: 'C'}}  # 声明动态维度，默认输入维度固定，可以声明为可变维度（用字符串占位符表示）
+	dynamic_axes={'input_ids': {0: 'B', 1: 'C'}, 'attention_mask': {0: 'B', 1: 'C'}}  # 声明动态维度，默认输入维度固定，可以声明为可变维度（用字符串占位符表示）
 )
 
 
