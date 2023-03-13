418da37a06375f4cabb0f58f1678ea33df0e040f
jamsluo <jamsluo@tencent.com>
2023-03-13 18:13:04 +0800

    init http client

--- trl/models/modeling_value_head.py
+++ trl/models/modeling_value_head.py
@@ -254,7 +254,7 @@ class AutoModelForSeq2SeqLMWithValueHead(PreTrainedModelWrapper):
         self.v_head = ValueHead(self.pretrained_model.config, **v_head_kwargs)
 
         self._init_weights(**v_head_kwargs)
-        self.triton_client = httpclient.InferenceServerClient(url="10.212.207.33:8000", connection_timeout= 300, network_timeout=300)
+        self.triton_client = httpclient.InferenceServerClient(url="10.212.204.89:8000", connection_timeout= 300, network_timeout=300)
     def set_tokenizer(self, tokenizer):
         self.tokenizer = tokenizer
 
