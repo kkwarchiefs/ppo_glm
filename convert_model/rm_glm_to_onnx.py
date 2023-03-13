1b6283ab7f5827b4caef7c70b24fecb8533df3e0
jamsluo <jamsluo@tencent.com>
2023-03-13 11:12:45 +0800

    init http client

--- convert_model/rm_glm_to_onnx.py
+++ convert_model/rm_glm_to_onnx.py
@@ -20,16 +20,16 @@ RM_model_path = "/search/ai/kaitongyang/RLHF_DEBUG/RM/summarization_reward_model
 RM_model_path = '/search/ai/pretrain_models/roberta-base-finetuned-jd-binary-chinese'
 RM_model_path = "/search/ai/kaitongyang/RLHF_DEBUG/RM/reward_model_glm_10b/final"
 
-RM_tokenizer = AutoTokenizer.from_pretrained(RM_model_path)
-RM_model = AutoModelForSequenceClassification.from_pretrained(RM_model_path, torchscript=True)
-RM_model.to(device)
-query_text = '什么人不能喝三七粉'
+RM_tokenizer = AutoTokenizer.from_pretrained(RM_model_path, trust_remote_code=True)
+RM_model = AutoModelForSequenceClassification.from_pretrained(RM_model_path, torchscript=True, trust_remote_code=True)
+RM_model = RM_model.half().eval()
+query_text = '什么人不能喝三七粉' + "[UNUSED1]"
 response_text = '服用三七粉期间,孕妇和儿童不宜使用。 三七粉是处方药,不是药品。 过量服用会引起中毒。'
-input = RM_tokenizer(query_text, response_text, truncation=True, max_length=512, padding="max_length", return_tensors="pt").to(device)
+input = RM_tokenizer(query_text + response_text, max_length=512, padding="max_length", return_tensors="pt").to(device)
 print(input)
-print(RM_model(input['input_ids'], input['attention_mask'], input['token_type_ids']))
+print(RM_model(input['input_ids'], input['attention_mask']))
 RM_model = RM_model.eval()  # 转换为eval模式
-inputs = (input['input_ids'], input['attention_mask'], input['token_type_ids'])  # 模型测试输入数据
+inputs = (input['input_ids'], input['attention_mask'])  # 模型测试输入数据
 os.makedirs(f"model_store/{model_name}/1", exist_ok=True)
 torch.onnx.export(
 	RM_model,
