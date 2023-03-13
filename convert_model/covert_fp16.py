6a53e2499490bdc39332d97a46ea41aa614d2a90
jamsluo <jamsluo@tencent.com>
2023-03-13 10:58:57 +0800

    init http client

--- convert_model/covert_fp16.py
+++ convert_model/covert_fp16.py
@@ -12,6 +12,6 @@ model_name = sys.argv[1]
 output_onnx_name = f"model_store/{model_name}/1/model.onnx"
 os.makedirs(f"model_store/{model_name}_fp16/1", exist_ok=True)
 
-onnx_model = load_model(output_onnx_name, load_external_data=False)
+onnx_model = load_model(output_onnx_name)
 trans_model = float16_converter.convert_float_to_float16(onnx_model ,keep_io_types=True)
 save_model(trans_model, f"model_store/{model_name}_fp16/1")
