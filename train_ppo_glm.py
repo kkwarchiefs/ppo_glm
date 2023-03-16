b853c965f8567603df650a44b885fd7afac4bfa8
jamsluo <jamsluo@tencent.com>
2023-03-16 18:16:28 +0800

    init http client

--- train_ppo_glm.py
+++ train_ppo_glm.py
@@ -122,7 +122,7 @@ config = PPOConfig(
     batch_size=16,
     ppo_epochs=1,
     # log_with="wandb",
-    init_kl_coef=0.2,
+    init_kl_coef=0.5,
     remove_unused_columns=False,
     mini_batch_size=16
 )
