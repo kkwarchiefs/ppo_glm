3d1da92c053bbfceef6462ad95c670920d5f6e25
jamsluo <jamsluo@tencent.com>
2023-03-18 23:53:57 +0800

    init

--- trl/models/modeling_value_head.py
+++ trl/models/modeling_value_head.py
@@ -178,7 +178,7 @@ class AutoModelForCausalLMWithValueHead(PreTrainedModelWrapper):
         last_hidden_state = base_model_output.hidden_states[-1]
         lm_logits = base_model_output.logits
         loss = base_model_output.loss
-        last_hidden_state.detach_()
+        # last_hidden_state.detach_()
         value = self.v_head(last_hidden_state).squeeze(-1)
 
         # force upcast in fp32 if logits are in half-precision
@@ -368,6 +368,7 @@ class AutoModelForSeq2SeqLMWithValueHead(PreTrainedModelWrapper):
         #last_hidden_state = base_model_output.mems[-1]
         #print(last_hidden_state.size())
         lm_logits = base_model_output.logits[:,input_ids.size()[1]-1:-1,:]
+        last_hidden_state.detach_()
         value = self.v_head(last_hidden_state).squeeze(-1)
 
         # force upcast in fp32 if logits are in half-precision
