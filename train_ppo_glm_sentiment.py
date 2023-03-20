5d3ed484e4ca9a4abea6b692f625d60e756570c0
jamsluo <jamsluo@tencent.com>
2023-03-20 11:15:26 +0800

    init http client

--- train_ppo_glm_sentiment.py
+++ train_ppo_glm_sentiment.py
@@ -157,10 +157,10 @@ def collator(data):
 
 # set seed before initializing value head for deterministic eval
 # set_seed(0)
-# print("os LOCAL_RANK", os.environ["LOCAL_RANK"])
-# if int(os.environ["LOCAL_RANK"]) % 2 == 1:
-#     print("sleep some time" )
-#     time.sleep(80)
+print("os LOCAL_RANK", os.environ["LOCAL_RANK"])
+if int(os.environ["LOCAL_RANK"]) % 2 == 1:
+    print("sleep some time" )
+    time.sleep(80)
 
 # Now let's build the model, the reference model, and the tokenizer.
 time.sleep(int(os.environ["LOCAL_RANK"]))
